6c5d755f408064c89a850bd966a8c8ffae3af3e7
iuryc <iurycauan0352@hotmail.com>
2022-07-20 08:14:46 -0300

    ok

---
+++ 04 - JS/08 - Function/arrowFunction.js
@@ -0,0 +1,11 @@
+const doubleVelocity = (velocity) => {
+    return velocity * 2
+}
+
+const TripleVelocity = (velocity) => velocity * 3
+
+
+console.log(doubleVelocity(20))
+console.log(TripleVelocity(3))
+
+
---
+++ 04 - JS/09 - Array/array.js
@@ -0,0 +1,6 @@
+let hitchedSpaceships = ["Supernova", "Elemental", "Helmet"]
+
+console.log(hitchedSpaceships[1])
+
+let spaceshipInfo = ["Colossus", 7, true]
+console.log(spaceshipInfo[1])
