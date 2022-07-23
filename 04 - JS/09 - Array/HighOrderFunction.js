3accc67b4f88a11c1f893ae355b3fe55de96bcd1
iuryc <iurycauan0352@hotmail.com>
2022-07-23 14:59:17 -0300

    ok

--- 04 - JS/09 - Array/HighOrderFunction.js
+++ 04 - JS/09 - Array/HighOrderFunction.js
@@ -1,9 +1,13 @@
 const vetor = [1, 2, 3]
 
-vetor.forEach((item, index) => console.log(item, index))
+vetor.forEach((item, index) => console.log("Array: " + item + "\nIndex: " + index))
 
-function mathOrder(num1, num2, op) {
+function mathOperator(num1, num2, op) {
     return op(num1, num2)
   }
-  
   console.log(mathOperator(1, 2, (num1, num2) => num1 + num2))
+
+  function operadorMult(num1, num2){
+    return num1 * num2
+  }
+  console.log(mathOperator(5, 2, operadorMult))
