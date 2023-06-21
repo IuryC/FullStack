dae1cdc82d51c04c4ccad7097536fb1cbf47afe4
IuryC <iurycauan0352@hotmail.com>
2023-06-21 07:07:02 -0300

    ok

---
+++ JS_2023/Condicionais/If_else/01.js
@@ -0,0 +1,12 @@
+let velocity = 90
+
+if(velocity < 40) {
+    console.log("Velocidade Baixa")
+} else if(velocity <= 100){
+        console.log("Cuidado! Próximo a velocidade de risco")
+    } else {
+        console.log("Velocidade de Risco")
+    }
+
+
+    
