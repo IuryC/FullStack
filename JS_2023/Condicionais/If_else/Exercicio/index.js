67385a5fb909801fb575049e9ce42176b8b71229
IuryC <iurycauan0352@hotmail.com>
2023-06-25 17:58:06 -0300

    ok

--- JS_2023/Condicionais/If_else/Exercicio/index.js
+++ JS_2023/Condicionais/If_else/Exercicio/index.js
@@ -6,30 +6,26 @@ let newVelocity = prompt("A que velocidade você gostaria de acelerar?")
 
 let confirmVelocity = confirm("Estamos acelerando para" + newVelocity + "km/s")
 
-if(confirmVelocity){
+if (confirmVelocity) {
     velocity = newVelocity
 }
 
-if(velocity <=0){
-    console.log("Nave está parada, considere partir")
+if (velocity <= 0) {
+    alert("Nave está parada, considere partir")
 } else {
-    if(velocity < 40){
-        console.log("Você está devagar, considere aumentar um pouco")
+    if (velocity < 40) {
+        alert("Você está devagar, considere aumentar um pouco")
+    } else if (velocity >= 40 && velocity < 80) {
+        alert("Parece uma bola velocidade para se manter")
+    } else if (velocity >= 80 && velocity < 100) {
+        alert("Velocidade alta, considere diminuir")
     } else {
-        if(velocity >= 40 && velocity < 80){
-            console.log("Parece uma bola velocidade para se manter")
-        } else {
-            if(velocity >= 80 && velocity < 100){
-                console.log("Velocidade alta, considere diminuir")
-            } else{
-                if(velocity >= 100){
-                    console.log("Velocidade Perigosa, controle autómatico forçado")
-                }
-               
-            }
-        }
+        alert("Velocidade Perigosa, controle autómatico forçado")
     }
+
 }
 
-alert(pilotName)
-alert(velocity)
+
+
+
+alert("Nome do Piloto: " + pilotName + "\nVelocidade: " + velocity)
