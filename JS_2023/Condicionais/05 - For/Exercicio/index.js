7c3a017f8cd0005dd5e3089d9cf7aead5e7807c1
IuryC <iurycauan0352@hotmail.com>
2023-06-27 13:12:06 -0300

    ok

--- JS_2023/Condicionais/05 - For/Exercicio/index.js
+++ JS_2023/Condicionais/05 - For/Exercicio/index.js
@@ -1,1 +1,13 @@
 let spaceshipName = prompt("Insira o nome da espaçonave: ")
+let newSpaceship = ""
+let caracterDiff = prompt("Qual caractere você deseja substituir?")
+let caracterSubs = prompt("Por qual caractere você deseja substituir?")
+
+for(let i = 0; i < spaceshipName.length; i++){
+    if(spaceshipName[i] == caracterDiff){
+        newSpaceship += caracterSubs 
+    } else {
+        newSpaceship += spaceshipName[i]
+    }
+}
+console.log(newSpaceship)
