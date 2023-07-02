8c45c396ae50495f5194eb71d58acf0e9e6ea672
IuryC <iurycauan0352@hotmail.com>
2023-07-02 18:18:46 -0300

    ok

--- JS_2023/09 - Function/07 - Função anon/index.js
+++ JS_2023/09 - Function/07 - Função anon/index.js
@@ -5,3 +5,11 @@ let doubleSpeed = function(velocity){
 let newVelocity = doubleSpeed(30)
 
 console.log(newVelocity)
+
+function speedUp(velocity, acceleration){
+    return velocity + acceleration
+}
+
+let newSpeed = speedUp(40, 30)
+
+console.log(newSpeed)
