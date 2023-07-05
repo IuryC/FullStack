3dc51f1435b371d02030fd8db0e0907dbd6b4dfc
IuryC <iurycauan0352@hotmail.com>
2023-07-05 13:23:42 -0300

    ok

--- JS_2023/10 - Array/02 - Funções básicas/index.js
+++ JS_2023/10 - Array/02 - Funções básicas/index.js
@@ -8,6 +8,12 @@ let removedSpaceship = hitchedSpaceships.pop() //retira o último array e retorn
 console.log(hitchedSpaceships)
 console.log("array removido: " + removedSpaceship)
 
-removedSpaceship = hitchedSpaceships.shift()
+removedSpaceship = hitchedSpaceships.shift() // retira o primeiro array
 console.log(hitchedSpaceships)
 console.log("Array removido: " + removedSpaceship) 
+
+hitchedSpaceships.unshift("Sunny") // adiciona array no inicio
+console.log(hitchedSpaceships, hitchedSpaceships.length) // .lenght imprime o tamanho do array
+
+let elementalPos = hitchedSpaceships. indexOf("Elemental") // Identifica a posição do array quando encontra o elemento, do contrário: -1
+console.log(elementalPos)
