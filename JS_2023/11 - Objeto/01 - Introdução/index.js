bdcea9df497259a74d1b3c6e06fbb61381872aa9
IuryC <iurycauan0352@hotmail.com>
2023-07-05 21:21:51 -0300

    ok

---
+++ JS_2023/11 - Objeto/01 - Introdução/index.js
@@ -0,0 +1,10 @@
+let spaceship = {}
+
+console.log(spaceship)
+
+console.log(spaceship.type)
+console.log(spaceship["name"])
+
+spaceship.isHitched = false
+spaceship["shieldLeve"] = 100
+console.log(spaceship)
