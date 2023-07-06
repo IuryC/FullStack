33d300d9313bcc697eb43c09cc2dbce2c15fde69
IuryC <iurycauan0352@hotmail.com>
2023-07-06 08:44:50 -0300

    ok

---
+++ JS_2023/11 - Objeto/00 - Exercicio/index.js
@@ -0,0 +1,13 @@
+let spaceship = {}
+
+let = initialVelocity = 0
+let = speedUp
+spaceship.name = prompt("Insira o nome da nave: ")
+spaceship.type = prompt("Insira o tipo da nave: ")
+spaceship.maxVelocity = prompt("Defina a velocidade máxima da nave: ")
+spaceship.choseOption = prompt("Escolha o número da opção desejada \n1 - Acelerar \n2 - Parar")
+
+switch(choseOption){
+    case 1:
+        
+}
