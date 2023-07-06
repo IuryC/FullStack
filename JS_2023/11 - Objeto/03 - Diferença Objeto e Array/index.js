d2235524932243afa93d690c0df11b3b7b83ce6a
IuryC <iurycauan0352@hotmail.com>
2023-07-06 08:12:19 -0300

    ok

---
+++ JS_2023/11 - Objeto/03 - Diferença Objeto e Array/index.js
@@ -0,0 +1,8 @@
+let spacialStation = {
+    name: "Receptora",
+    plataformsQuantity: 15,
+    shape: "Esferica"
+}
+
+let spaceshipNames = ["Fenix", "Puller", "Golias"]
+console.log(spaceshipNames[0].toUpperCase())
---
+++ JS_2023/11 - Objeto/04 - Aninhando Objeto e Array/index.js
@@ -0,0 +1,18 @@
+
+let spaceship = {
+    name: "Supernova",
+    type: "Batalha",
+    crew: ["Cap. Silva", "Ana Júlia", "Thiago"]
+}
+
+spaceship.crew.push("Ten. Fernanda")
+
+console.log(spaceship)
+
+let spaceships = [
+    { name: "Elemental", crewQuantity: 10 },
+    { name: "Colossus", crewQuantity: 8 },
+    { name: "Helmet", crewQuantity: 15}
+]
+
+console.log(spaceships[1].crewQuantity)
