0c5646e7072a039658f3e7fd850c66c79ed695d1
IuryC <iurycauan0352@hotmail.com>
2023-07-07 09:59:45 -0300

    ok

---
+++ JS_2023/12 - POO/02 - Construtores e Instância/index.js
@@ -0,0 +1,9 @@
+class SpacialStation {
+    constructor(name, plataformsQuantity) {
+        this.name = name
+        this.plataformsQuantity = plataformsQuantity
+    }
+}
+
+let observatory = new SpacialStation("Observatório", 40)
+console.log(observatory)
