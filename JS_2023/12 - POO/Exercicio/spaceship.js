340ee55d6201b3636df3b5d1d4d2053ead910067
IuryC <iurycauan0352@hotmail.com>
2023-07-08 10:39:53 -0300

    ok

--- JS_2023/12 - POO/Exercicio/spaceship.js
+++ JS_2023/12 - POO/Exercicio/spaceship.js
@@ -1,15 +1,26 @@
 class Spaceship {
-    static get DECELERATION_RATE(){
-        return 0.17
+    static get DECELERATION_RATE() {
+      return 0.17
     }
-
+  
     constructor(name, crewQuantity) {
-        this.name = name
-        this.crewQuantity = crewQuantity
-        this.currentVelocity = 0 
+      this.name = name
+      this.crewQuantity = crewQuantity
+      this.currentVelocity = 0
     }
-
-    speedUp(acceleration){
-        this.currentVelocity += acceleration * (1 - Spaceship.DECELERATION_RATE)
+  
+    speedUp(acceleration) {
+      this.currentVelocity += acceleration * (1 - Spaceship.DECELERATION_RATE)
     }
-}
+  }
+  
+  
+  battle_spaceship.js
+  
+  class BattleSpaceship extends Spaceship {
+    constructor(name, crewQuantity, weaponsQuantity) {
+      super(name, crewQuantity)
+      this.weaponsQuantity = weaponsQuantity
+    }
+  }
+  
