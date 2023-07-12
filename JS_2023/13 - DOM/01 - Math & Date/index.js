ad23d0d3748bd3b491bfcbcc76573d5cb8e041f2
IuryC <iurycauan0352@hotmail.com>
2023-07-12 09:05:55 -0300

    ok

---
+++ JS_2023/13 - DOM/01 - Math & Date/index.js
@@ -0,0 +1,13 @@
+console.log(Math.pow(5, 3))
+
+console.log(Math.PI)
+
+console.log(Math.sqrt(64))
+
+let myBirthday = new Date(1999, 3, 4)
+
+console.log(myBirthday) // O primeiro mês é 0 = janeiro
+
+let today = Date.now()
+
+console.log(today)
