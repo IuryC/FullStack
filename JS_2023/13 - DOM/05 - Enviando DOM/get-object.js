e028c99b963b966ee3809fa4ebbe26dd5da7f1c6
IuryC <iurycauan0352@hotmail.com>
2023-07-13 09:50:08 -0300

    ok

---
+++ JS_2023/13 - DOM/05 - Enviando DOM/get-object.js
@@ -0,0 +1,8 @@
+function sayMyFirstName(element) {
+    alert("Meu primeiro nome é: " + element.value)
+}
+
+function sayMyLastName(element) {
+    console.log(event)
+    alert("Meu último nome é " + element.target.value)
+}
