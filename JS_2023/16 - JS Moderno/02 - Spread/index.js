493c8bb05e5bc71726ab92114d68c404df1b01c1
IuryC <iurycauan0352@hotmail.com>
2023-08-04 10:54:36 -0300

    ok

---
+++ JS_2023/16 - JS Moderno/02 - Spread/index.js
@@ -0,0 +1,14 @@
+let spaceships = ["Colossus", "Artemis", "Fenix"]
+
+let newSpaceships = [...spaceships, "Puller"]
+
+console.log(newSpaceships)
+
+function speedUp(velocity, acceleration) {
+    return velocity + acceleration
+}
+
+let spaceshipAcceleration = [60, 10]
+
+let newVelocity = speedUp(...spaceshipAcceleration)
+console.log(newVelocity)
