3a0edd12cb81719bd0a9aea784f935bbf9998308
IuryC <iurycauan0352@hotmail.com>
2023-08-08 10:57:42 -0300

    ok

--- 09 - WEBPACK/src/index.js
+++ 09 - WEBPACK/src/index.js
@@ -1,9 +1,3 @@
 import moment from "moment/src/moment"
 
 alert(moment().format("HH:mm:ss"))
-
-module.exports = {
-    mode: 'development', // ou 'production'
-    // ... outras configurações
-  };
-  
